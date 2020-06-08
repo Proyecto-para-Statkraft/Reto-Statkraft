90e6088d7279d7053d101f7b0df60a26b447249c
eliza29 <elizaseguraf@gmail.com>
2020-06-08 01:00:46 -0500

    cdn-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import './bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <button type="button" className="btn btn-light">Light</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
