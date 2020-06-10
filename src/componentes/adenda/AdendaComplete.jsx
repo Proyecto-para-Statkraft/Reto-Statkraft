59392f1b4fac1f48f3bbb75d3dabd7d8d2b16fba
eliza29 <elizaseguraf@gmail.com>
2020-06-10 00:16:51 -0500

    llenando data primera seccion adenda

--- src/componentes/adenda/AdendaComplete.jsx
+++ src/componentes/adenda/AdendaComplete.jsx
@@ -6,28 +6,9 @@ import FirstSection from "./FirstSection";
 import SecondSection from "./SecondSection";
 import ThirdSection from "./ThirdSection";
 
-// const adendaData = {
-//     numberAdenda: 'PRIMERA',
-//     nameContract: 'COMPRAVENTA E INSTALACIÓN SKP-450026438',
-//     representativeOne: 'Álvaro Antonio PorturasIngunza,',
-//     dniOne: '09340737',
-//     representativeTwo: 'Miguel Antonio Castro Peña,',
-//     dniTwo: '40286317',
-//     business: 'IBM DEL PERÚ S.A.C.,',
-//     rucBusiness: ' 20100075009,',
-//     directionBusiness: 'n Av. Javier Prado Este 6230, LaRiviera de Monterrico, La Molina',
-//     representativeBusiness: ' María Eugenia Basauri Parra',
-//     dniBusiness: '10613966',
-//     partidaElectronica: '11013475',
-//     nameX: '“LOCADOR”',
-
-
-// }
-
-
 function AdendaComplete() {
 
-    const [spells, setSpells] = useState([]);
+    const [oneAdenda, setAdenda] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -35,31 +16,35 @@ function AdendaComplete() {
             const data = await db.collection("adendaData")
                 .onSnapshot(
                     snapShots => {
-                        setSpells(snapShots.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+                        setAdenda(snapShots.docs.map(doc => ({ ...doc.data(), id: doc.id })))
                     })
             console.log(data)
         };
         fetchData();
     }, []);
 
-    console.log(spells)
+    console.log(oneAdenda)
 
     return (
         <div>
-        {spells.map((spell) => 
-            <AdendaIntroduction key={spell.id} numberAdenda={spell.numberAdenda} nameContract={spell.nameContract}
-                representativeOne={spell.representativeOne} dniOne={spell.dniOne}
-                representativeTwo={spell.representativeTwo} dniTwo={spell.dniTwo}
-                business={spell.business} rucBusiness={spell.rucBusiness} directionBusiness={spell.directionBusiness}
-                representativeBusiness={spell.representativeBusiness} dniBusiness={spell.dniBusiness}
-                partidaElectronica={spell.partidaElectronica} nameX={spell.nameX} />
-           
-        )}
-            <FirstSection/>
+        {oneAdenda.map((propertieAdenda) => 
+        <div key={propertieAdenda.id}>
+            <AdendaIntroduction numberAdenda={propertieAdenda.numberAdenda} typeContract={propertieAdenda.typeContract} codeContract={propertieAdenda.codeContract}
+                representativeOne={propertieAdenda.representativeOne} dniOne={propertieAdenda.dniOne}
+                representativeTwo={propertieAdenda.representativeTwo} dniTwo={propertieAdenda.dniTwo}
+                business={propertieAdenda.business} rucBusiness={propertieAdenda.rucBusiness} directionBusiness={propertieAdenda.directionBusiness}
+                representativeBusiness={propertieAdenda.representativeBusiness} dniBusiness={propertieAdenda.dniBusiness}
+                partidaElectronica={propertieAdenda.partidaElectronica} nameX={propertieAdenda.nameX} />
+            <FirstSection dayContract={propertieAdenda.dayContract} monthContract={propertieAdenda.monthContract}
+                yearContract={propertieAdenda.yearContract} detailObject={propertieAdenda.detailObject}
+                dayAdenda={propertieAdenda.dayAdenda} monthAdenda={propertieAdenda.monthAdenda}
+                yearAdenda={propertieAdenda.yearAdenda} detailObject2={propertieAdenda.detailObject2}
+                />
             <SecondSection/>
             <ThirdSection/> 
-          
-
+            </div>
+        )}
+            
         </div >
     )
 }
--- src/componentes/adenda/FirstSection.jsx
+++ src/componentes/adenda/FirstSection.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 
-const info = {
-    info1:'espacio',
-}
 
-const FirstSection = () =>{
+const FirstSection = (props) =>{
     return(
         <div>
-            <h5>PRIMERA ANTECEDENTES</h5>
+            <h5><span>PRIMERA</span> ANTECEDENTES</h5>
             <p>
-                1 Con fecha {info.info1} de [●] de [●], las Partes suscribieron el Contrato 
-                de [●](en adelante, el “Contrato”), mediante el cual [DETALLAR OBJETO].
+                <span>1.1</span>Con fecha {props.dayContract} de {props.monthContract} de {props.yearContract}, las Partes suscribieron el Contrato 
+                de {props.typeContract} (en adelante, el “Contrato”), mediante el cual {props.detailObject}
             </p>
             <p>
-                [Con fecha [●] de [●] de [●], las Partes suscribieron la Primera Adenda al Contrato (en adelante, la “Primera Adenda”), mediante la cual [DETALLAR OBJETO].]
-                [Nota: De ser el caso, hacer breve referencia a las adendas que se hubieran suscrito.]
+                <span>1.2</span>[Con fecha {props.dayAdenda} de {props.monthAdenda} de {props.yearAdenda}, las Partes suscribieron la Primera Adenda al Contrato (en adelante, la “Primera Adenda”), 
+                mediante la cual {props.detailObject2}
             </p>
         </div>
     )
