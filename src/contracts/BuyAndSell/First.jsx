781328755cf1012f906c4be6062fdd0c5f7b9d0e
MariangelMM <mariangelmoramarquez@gmail.com>
2020-06-10 18:50:31 -0500

    rellenando contrato

--- src/contracts/BuyAndSell/First.jsx
+++ src/contracts/BuyAndSell/First.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React from "react";
 
+// const sell = {
+//   product: '',
+//   place: ''
+// }
 
-const sell = {
-  product: '',
-  place: ''
-}
+const First = ({ data, First }) => {
+  const { companyData } = data;
 
+  // console.log(sell);
 
-const First = ({ companyData, sell }) => (
-  <section>
-    <h3>PRIMERA:         ANTECEDENTES</h3>
-    <p>
-      1.1 {companyData.name} es una empresa dedicada a la generación y comercialización
-        de energía eléctrica, que requiere {sell.product} en {sell.place}.
-      </p>
-    <p>
-      1.2 El <span>PROVEEDOR</span> es una empresa contratista independiente dedicada, entre
-      otras cosas, a la compraventa de bienes como los requeridos por {companyData.name},
-      para lo cual cuenta con la experiencia, personal propio, autonomía técnica,
-      económica y operativa que lo hacen apto para proporcionar los bienes materia
-      del Contrato.
-    </p>
-  </section>
-);
-
+  if (First !== "") {
+    return (
+      <section>
+        <h3>PRIMERA: ANTECEDENTES</h3>
+        <p>
+          1.1 {companyData.name} es una empresa dedicada a la generación y
+          comercialización de energía eléctrica, que requiere {First.product} en
+          {First.place}.
+        </p>
+        <p>
+          1.2 El <span>PROVEEDOR</span> es una empresa contratista independiente
+          dedicada, entre otras cosas, a la compraventa de bienes como los
+          requeridos por {companyData.name}, para lo cual cuenta con la
+          experiencia, personal propio, autonomía técnica, económica y operativa
+          que lo hacen apto para proporcionar los bienes materia del Contrato.
+        </p>
+      </section>
+    );
+  }
+  return (
+    <div>
+      <p>Seccion Antecedentes</p>
+    </div>
+  )
+};
 
 export default First;
