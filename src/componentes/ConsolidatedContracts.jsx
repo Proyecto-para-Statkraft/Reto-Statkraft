982e908bc518fe61677141e45782a9d9af4889e1
yamiquispe <yamipqm@gmail.com>
2020-06-10 19:01:26 -0500

    Instalando react-bootstrap

--- src/componentes/ConsolidatedContracts.jsx
+++ src/componentes/ConsolidatedContracts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Table from 'react-bootstrap/Table';
 
 const ConsolidatedContracts = () => {
     return (
@@ -7,7 +8,35 @@ const ConsolidatedContracts = () => {
         <div>
             <Header />
             <p>Consolidados Contratos</p>
-
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     )
 }
