55526b103a95c09a66a742ecd038b1b388552184
yamiquispe <yamipqm@gmail.com>
2020-06-12 04:55:39 -0500

    Borrando espaciado

--- src/componentes/escogerempresa/Welcome.jsx
+++ src/componentes/escogerempresa/Welcome.jsx
@@ -4,7 +4,6 @@ import Header from '../Header';
 
 const Welcome = () => {
 
-
     const [nameServices, setnameServices] = useState("");
     const nameService = e => { setnameServices(e.target.value) };
 
