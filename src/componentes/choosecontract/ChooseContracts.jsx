8f170f73f1064e4ada028c1b3d6524c7ad4ee14a
eliza29 <elizaseguraf@gmail.com>
2020-06-12 03:22:21 -0500

    modificacion css

--- src/componentes/choosecontract/ChooseContracts.jsx
+++ src/componentes/choosecontract/ChooseContracts.jsx
@@ -18,17 +18,17 @@ const ReviewContracts = () => {
             <Header />
             <div className="mt-5 d-flex justify-content-center">
                 <div className="m-sm-1 col-sm-7">
-                    <h5 className="mb-5">1- ¿Qué es lo que vas a contratar?</h5>
-                    <div className="m-3" onChange={optionContracts}>
-                        <input className="mb-3 radio" type="radio" value="bienes" name="gender" /> Adquisición de Bienes <br />
-                        <input className="mb-3 radio" type="radio" value="transporte" name="gender" /> Trasporte Personal<br />
-                        <input className="mb-3 radio" type="radio" value="vigilancia" name="gender" /> Vigilancia, limpieza o jardineria en nuestras instalaciones <br />
-                        <input className="mb-3 radio" type="radio" value="alquiler" name="gender" /> Alquiler <br />
-                        <input className="mb-3 radio" type="radio" value="construccion" name="gender" /> Construcción de una infrastructura <br />
-                        <input className=" radio" type="radio" value="otro" name="gender" /> Otro tipo no descrito antes
+                    <h5 className="mb-5">1. ¿Qué es lo que vas a contratar?</h5>
+                    <div className="m-3 vista-radios" onChange={optionContracts}>
+                        <input className="mb-3 radio" type="radio" value="bienes" name="gender" /> <span className="ml-3">Adquisición de Bienes</span> <br />
+                        <input className="mb-3 radio" type="radio" value="transporte" name="gender" /> <span className="ml-3">Trasporte Personal</span><br />
+                        <input className="mb-3 radio" type="radio" value="vigilancia" name="gender" /><span className="ml-3">Vigilancia, limpieza o jardineria en nuestras instalaciones </span> <br />
+                        <input className="mb-3 radio" type="radio" value="alquiler" name="gender" /> <span className="ml-3">Alquiler </span><br />
+                        <input className="mb-3 radio" type="radio" value="construccion" name="gender" /> <span className="ml-3">Construcción de una infrastructura</span> <br />
+                        <input className=" radio" type="radio" value="otro" name="gender" /><span className="ml-3">Otro tipo no descrito antes</span> 
                     </div>
                     <div className=" d-flex justify-content-end ">
-                        <Link className="btn btn-primary m-3 "
+                        <Link className="col-sm-3  btn btn-primary"
                             to={{
                                 pathname: "/questions",
                                 selectedOption,
